0b92599d359669e90fc00ebfd71cc0d70131ce1f
Hanin Sherif <136843019+HaninSh@users.noreply.github.com>
2024-10-20 23:09:43 +0300

    Update flex readings
    subs max & min ranges with avg value

--- services/get_word_sound/word_sound_cfg.h
+++ services/get_word_sound/word_sound_cfg.h
@@ -5,32 +5,32 @@
 
 
 
-#define SEN_1_WORD_1	{(uint16_)135, (uint16_)155, (uint16_)125, (uint16_)145, (uint16_)217, (uint16_)237, (uint16_)157, (uint16_)177, (uint16_)197, (uint16_)217, (uint16_)1, (uint16_)1, (uint8_t)1, (uint8_t)1}
-#define SEN_1_WORD_2	{(uint16_)146, (uint16_)166, (uint16_)149, (uint16_)169, (uint16_)189, (uint16_)209, (uint16_)153, (uint16_)173, (uint16_)143, (uint16_)163, (uint16_)0, (uint16_)1, (uint8_t)1, (uint8_t)2}
+#define SEN_1_WORD_1	{(uint16_)145, (uint16_)135, (uint16_)227, (uint16_)167, (uint16_)207, (uint16_)1, (uint16_)1, (uint8_t)1, (uint8_t)1}
+#define SEN_1_WORD_2	{(uint16_)156, (uint16_)159, (uint16_)199, (uint16_)163, (uint16_)153, (uint16_)0, (uint16_)1, (uint8_t)1, (uint8_t)2}
 
-#define SEN_2_WORD_1	{(uint16_)166, (uint16_)186, (uint16_)143, (uint16_)163, (uint16_)169, (uint16_)189, (uint16_)165, (uint16_)185, (uint16_)157, (uint16_)177, (uint16_)0, (uint16_)1, (uint8_t)2, (uint8_t)1}
+#define SEN_2_WORD_1	{(uint16_)176, (uint16_)153, (uint16_)179, (uint16_)175, (uint16_)167, (uint16_)0, (uint16_)1, (uint8_t)2, (uint8_t)1}
 
-#define SEN_3_WORD_1	{(uint16_)174, (uint16_)194, (uint16_)137, (uint16_)157, (uint16_)174, (uint16_)194, (uint16_)174, (uint16_)194, (uint16_)149, (uint16_)169, (uint16_)0, (uint16_)1, (uint8_t)3, (uint8_t)1}
-#define SEN_3_WORD_2	{(uint16_)142, (uint16_)162, (uint16_)196, (uint16_)216, (uint16_)182, (uint16_)202, (uint16_)151, (uint16_)171, (uint16_)194, (uint16_)214, (uint16_)0, (uint16_)1, (uint8_t)3, (uint8_t)2}
-#define SEN_3_WORD_3	{(uint16_)166, (uint16_)186, (uint16_)133, (uint16_)153, (uint16_)230, (uint16_)250, (uint16_)157, (uint16_)177, (uint16_)189, (uint16_)209, (uint16_)0, (uint16_)1, (uint8_t)3, (uint8_t)3}
+#define SEN_3_WORD_1	{(uint16_)184, (uint16_)147, (uint16_)184, (uint16_)184, (uint16_)159, (uint16_)0, (uint16_)1, (uint8_t)3, (uint8_t)1}
+#define SEN_3_WORD_2	{(uint16_)152, (uint16_)206, (uint16_)192, (uint16_)161, (uint16_)204, (uint16_)0, (uint16_)1, (uint8_t)3, (uint8_t)2}
+#define SEN_3_WORD_3	{(uint16_)176, (uint16_)143, (uint16_)240, (uint16_)167, (uint16_)199, (uint16_)0, (uint16_)1, (uint8_t)3, (uint8_t)3}
 
-#define SEN_4_WORD_1	{(uint16_)199, (uint16_)219, (uint16_)117, (uint16_)137, (uint16_)245, (uint16_)265, (uint16_)206, (uint16_)226, (uint16_)194, (uint16_)214, (uint16_)0, (uint16_)1, (uint8_t)4, (uint8_t)1}
-#define SEN_4_WORD_2	{(uint16_)182, (uint16_)202, (uint16_)142, (uint16_)162, (uint16_)197, (uint16_)217, (uint16_)173, (uint16_)193, (uint16_)178, (uint16_)198, (uint16_)1, (uint16_)1, (uint8_t)4, (uint8_t)2}
-#define SEN_4_WORD_3	{(uint16_)166, (uint16_)186, (uint16_)143, (uint16_)163, (uint16_)162, (uint16_)182, (uint16_)162, (uint16_)182, (uint16_)146, (uint16_)166, (uint16_)0, (uint16_)1, (uint8_t)4, (uint8_t)3}
-#define SEN_4_WORD_4	{(uint16_)183, (uint16_)203, (uint16_)166, (uint16_)186, (uint16_)196, (uint16_)216, (uint16_)189, (uint16_)209, (uint16_)178, (uint16_)198, (uint16_)0, (uint16_)1, (uint8_t)4, (uint8_t)4}
+#define SEN_4_WORD_1	{(uint16_)209, (uint16_)127, (uint16_)255, (uint16_)216, (uint16_)204, (uint16_)0, (uint16_)1, (uint8_t)4, (uint8_t)1}
+#define SEN_4_WORD_2	{(uint16_)192, (uint16_)152, (uint16_)207, (uint16_)183, (uint16_)188, (uint16_)1, (uint16_)1, (uint8_t)4, (uint8_t)2}
+#define SEN_4_WORD_3	{(uint16_)176, (uint16_)153, (uint16_)172, (uint16_)172, (uint16_)156, (uint16_)0, (uint16_)1, (uint8_t)4, (uint8_t)3}
+#define SEN_4_WORD_4	{(uint16_)193, (uint16_)176, (uint16_)206, (uint16_)199, (uint16_)188, (uint16_)0, (uint16_)1, (uint8_t)4, (uint8_t)4}
 
-#define SEN_5_WORD_1	{(uint16_)206, (uint16_)226, (uint16_)188, (uint16_)208, (uint16_)215, (uint16_)235, (uint16_)201, (uint16_)221, (uint16_)212, (uint16_)232, (uint16_)0, (uint16_)1, (uint8_t)5, (uint8_t)1}
-#define SEN_5_WORD_2	{(uint16_)197, (uint16_)217, (uint16_)133, (uint16_)153, (uint16_)221, (uint16_)241, (uint16_)197, (uint16_)217, (uint16_)197, (uint16_)217, (uint16_)0, (uint16_)1, (uint8_t)5, (uint8_t)2}
-#define SEN_5_WORD_3	{(uint16_)214, (uint16_)234, (uint16_)133, (uint16_)153, (uint16_)196, (uint16_)216, (uint16_)185, (uint16_)205, (uint16_)194, (uint16_)214, (uint16_)1, (uint16_)1, (uint8_t)5, (uint8_t)3}
-#define SEN_5_WORD_4	{(uint16_)182, (uint16_)202, (uint16_)135, (uint16_)155, (uint16_)162, (uint16_)182, (uint16_)167, (uint16_)187, (uint16_)157, (uint16_)177, (uint16_)1, (uint16_)1, (uint8_t)5, (uint8_t)4}
+#define SEN_5_WORD_1	{(uint16_)216, (uint16_)198, (uint16_)225, (uint16_)211, (uint16_)222, (uint16_)0, (uint16_)1, (uint8_t)5, (uint8_t)1}
+#define SEN_5_WORD_2	{(uint16_)207, (uint16_)143, (uint16_)231, (uint16_)207, (uint16_)207, (uint16_)0, (uint16_)1, (uint8_t)5, (uint8_t)2}
+#define SEN_5_WORD_3	{(uint16_)224, (uint16_)143, (uint16_)206, (uint16_)195, (uint16_)408, (uint16_)1, (uint16_)1, (uint8_t)5, (uint8_t)3}
+#define SEN_5_WORD_4	{(uint16_)192, (uint16_)145, (uint16_)172, (uint16_)177, (uint16_)167, (uint16_)1, (uint16_)1, (uint8_t)5, (uint8_t)4}
 
-#define SEN_6_WORD_1	{(uint16_)167, (uint16_)187, (uint16_)135, (uint16_)155, (uint16_)164, (uint16_)184, (uint16_)167, (uint16_)187, (uint16_)162, (uint16_)182, (uint16_)1, (uint16_)1, (uint8_t)6, (uint8_t)1}
-#define SEN_6_WORD_2	{(uint16_)164, (uint16_)184, (uint16_)142, (uint16_)162, (uint16_)166, (uint16_)186, (uint16_)169, (uint16_)189, (uint16_)149, (uint16_)169, (uint16_)1, (uint16_)1, (uint8_t)6, (uint8_t)2}
+#define SEN_6_WORD_1	{(uint16_)177, (uint16_)145, (uint16_)174, (uint16_)177, (uint16_)172, (uint16_)1, (uint16_)1, (uint8_t)6, (uint8_t)1}
+#define SEN_6_WORD_2	{(uint16_)174, (uint16_)152, (uint16_)176, (uint16_)179, (uint16_)159, (uint16_)1, (uint16_)1, (uint8_t)6, (uint8_t)2}
 
-#define SEN_7_WORD_1	{(uint16_)199, (uint16_)219, (uint16_)117, (uint16_)137, (uint16_)245, (uint16_)265, (uint16_)206, (uint16_)226, (uint16_)194, (uint16_)214, (uint16_)0, (uint16_)1, (uint8_t)7, (uint8_t)1}
-#define SEN_7_WORD_2	{(uint16_)182, (uint16_)202, (uint16_)142, (uint16_)162, (uint16_)197, (uint16_)217, (uint16_)173, (uint16_)193, (uint16_)178, (uint16_)198, (uint16_)1, (uint16_)1, (uint8_t)7, (uint8_t)2}
-#define SEN_7_WORD_3	{(uint16_)172, (uint16_)192, (uint16_)142, (uint16_)162, (uint16_)199, (uint16_)219, (uint16_)196, (uint16_)216, (uint16_)180, (uint16_)200, (uint16_)0, (uint16_)1, (uint8_t)7, (uint8_t)3}
-#define SEN_7_WORD_4	{(uint16_)189, (uint16_)209, (uint16_)226, (uint16_)246, (uint16_)199, (uint16_)219, (uint16_)196, (uint16_)216, (uint16_)201, (uint16_)221, (uint16_)1, (uint16_)1, (uint8_t)7, (uint8_t)4}
+#define SEN_7_WORD_1	{(uint16_)209, (uint16_)127, (uint16_)255, (uint16_)216, (uint16_)204, (uint16_)0, (uint16_)1, (uint8_t)7, (uint8_t)1}
+#define SEN_7_WORD_2	{(uint16_)182, (uint16_)152, (uint16_)207, (uint16_)183, (uint16_)188, (uint16_)1, (uint16_)1, (uint8_t)7, (uint8_t)2}
+#define SEN_7_WORD_3	{(uint16_)182, (uint16_)152, (uint16_)209, (uint16_)206, (uint16_)190, (uint16_)0, (uint16_)1, (uint8_t)7, (uint8_t)3}
+#define SEN_7_WORD_4	{(uint16_)199, (uint16_)236, (uint16_)209, (uint16_)206, (uint16_)211, (uint16_)1, (uint16_)1, (uint8_t)7, (uint8_t)4}
 
 
 #define STOPPING_WORD	{(uint16_)170, (uint16_)190, (uint16_)145, (uint16_)165, (uint16_)180, (uint16_)200, (uint16_)182, (uint16_)202, (uint16_)180, (uint16_)200, (uint16_)0, (uint16_)1, (uint8_t)0, (uint8_t)0}
