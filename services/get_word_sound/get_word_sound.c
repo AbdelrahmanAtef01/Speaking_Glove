8e3280a1de49fd58a06471a47be205a7a84edd4c
Hanin Sherif <136843019+HaninSh@users.noreply.github.com>
2024-10-22 08:26:39 +0300

    Mapping function

--- services/get_word_sound/get_word_sound.c
+++ services/get_word_sound/get_word_sound.c
@@ -26,22 +26,44 @@ word words[TOTAL_NO_OF_WORDS] = {SEN_1_WORD_1, SEN_1_WORD_2, SEN_2_WORD_1, SEN_3
 									  SEN_7_WORD_1, SEN_7_WORD_2, SEN_7_WORD_3, SEN_7_WORD_4,
 									  STOPPING_WORD, NOT_FOUND};
 
-void GET_vidGetWordAndSound(uint16_ * sensor_reads, uint8_t * word_location )
-{
-	for (uint8_t i=0; i<TOTAL_NO_OF_WORDS-1; i++)
-	{
-		if(/*word mapping technique*/
-		   sensor_reads[5]==(uint16_)words[i].tilt0 && sensor_reads[6]==(uint16_)words[i].tilt1 )
-		{
-			word_location[0] = words[i].sentence;
-			word_location[1] = words[i].word;
-			break;
-		}else{
-			word_location[0] = words[TOTAL_NO_OF_WORDS-1].sentence;
-			word_location[1] = words[TOTAL_NO_OF_WORDS-1].word;
-		}
-	}
-
-
+// HELPING FUNCTIONS 
+// #1 Function to calculate Euclidean distance between two sensor readings
+float calculate_distance(uint16_t* sensor_reads, word* word) {
+    float distance = 0.0;
+    distance += pow((float)(sensor_reads[0] - word->thumb), 2);
+    distance += pow((float)(sensor_reads[1] - word->finger_1), 2);
+    distance += pow((float)(sensor_reads[2] - word->finger_2), 2);
+    distance += pow((float)(sensor_reads[3] - word->finger_3), 2);
+    distance += pow((float)(sensor_reads[4] - word->finger_4), 2);
+    distance += pow((float)(sensor_reads[5] - word->tilt0), 2);
+    distance += pow((float)(sensor_reads[6] - word->tilt1), 2);
+    return sqrt(distance);
 }
 
+
+// #2 Function to find the closest predefined sensor data
+int find_closest_sensor(word* words, uint16_t* sensor_reads, int num_words) {
+    int closest_index = 0;
+    float min_distance = calculate_distance(sensor_reads, &words[0]);
+
+    for (int i = 1; i < num_words; i++) {
+        float distance = calculate_distance(sensor_reads, &words[i]);
+        if (distance < min_distance) {
+            min_distance = distance;
+            closest_index = i;
+        }
+    }
+    return closest_index;
+}
+
+
+// Main Function 
+void GET_vidGetWordAndSound(uint16_t * sensor_reads, uint8_t * word_location )
+{
+	
+uint8_t i = find_closest_sensor(words, sensor_reads, TOTAL_NO_OF_WORDS);
+
+word_location[0] = words[i].sentence;
+word_location[1] = words[i].word;
+
+}
